22daf5a98e564092d7beeb1b25b9470ec1cdf168
Adrian Tache <contact@adriantache.com>
2018-03-27 20:56:44 +0300

    Add JSON parsing, modify Earthquake class as  appropriate

--- app/src/main/java/com/adriantache/quakereport/Earthquake.java
+++ app/src/main/java/com/adriantache/quakereport/Earthquake.java
@@ -1,31 +1,45 @@
 package com.adriantache.quakereport;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Custom class to save all relevant earthquake data
  */
 
 public class Earthquake {
 
-    private float magnitude;
+    private double magnitude;
     private String orientation;
     private String location;
     private String time;
 
-    public Earthquake(float magnitude, String orientation, String location, String time) {
+    public Earthquake(double magnitude, String orientation, String location, long time) {
         this.magnitude = magnitude;
         this.orientation = orientation;
         this.location = location;
-        this.time = time;
+        //convert time from UNIX time to a String
+        Date date = new Date(time);
+        SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        // use the timezone from the USGS
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("UTC"));
+        this.time = sdf.format(date);
     }
 
     //todo remove this constructor as we progress
-    public Earthquake(float magnitude, String location, String time) {
+    public Earthquake(double magnitude, String location, long time) {
         this.magnitude = magnitude;
         this.location = location;
-        this.time = time;
+        //convert time from UNIX time to a String
+        Date date = new Date(time);
+        SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        // use the timezone from the USGS
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("UTC"));
+        this.time = sdf.format(date);
     }
 
-    public float getMagnitude() {
+    public double getMagnitude() {
         return magnitude;
     }
 
