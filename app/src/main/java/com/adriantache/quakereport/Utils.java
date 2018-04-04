f858e4d2c63dc6aa8854183a292c0021429deeb3
Adrian Tache <contact@adriantache.com>
2018-04-04 16:02:01 +0300

    Implement http connectivity

---
+++ app/src/main/java/com/adriantache/quakereport/Utils.java
@@ -0,0 +1,76 @@
+package com.adriantache.quakereport;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import static android.content.ContentValues.TAG;
+
+public class Utils {
+
+    public static String makeHTTPConnection(String url) {
+        URL formedURL;
+        String jsonResponse = "";
+
+        try {
+            formedURL = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) formedURL.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.connect();
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else Log.e(TAG, "makeHTTPConnection: " + urlConnection.getResponseCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            try {
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "";
+            }
+        }
+        return output.toString();
+    }
+}
